a95a38afa7fc9b75b69e6ec52a4b8a7729ca6f72
Allan <allan.delatuillerie@gmail.com>
2024-06-27 11:04:37 +0200

    Commencement du home

--- src/App.jsx
+++ src/App.jsx
@@ -3,65 +3,28 @@
   import tabouret from './assets/tabouret.jpeg'
 
 function App() {
-
+const furnitures = furniture()
+let furArr = []
+  for(let i = 0; i < 6; i++) {
+    furArr.push(furnitures)
+  }
   return (
-
-    // todo: boucle
-      <div className='grid'>
-        <div className="card">
-          <img src={tabouret} alt="table basse 70's" />
-          <h1>Table basse 70's</h1>
-          <p>
-            cette table rose est super !
-          </p>
-          <button>Buy</button>
-        </div>
-
-        <div className="card">
-          <img src={tabouret} alt="tabouret 60's" />
-          <h1>Tabouret 60's</h1>
-          <p>
-            100% plastique
-          </p>
-          <button>Buy</button>
-        </div>
-
-        <div className="card">
-          <img src={tabouret} alt="armoire de mamie" />
-          <h1>Amoire de mamie</h1>
-          <p>
-            ça pue et c'est lourd mais c'est super solide
-          </p>
-          <button>Buy</button>
-        </div>
-
-        <div className="card">
-          <img src={tabouret} alt="table basse 70's" />
-          <h1>Table basse 70's</h1>
-          <p>
-            cette table rose est super !
-          </p>
-          <button>Buy</button>
-        </div>
-
-        <div className="card">
-          <img src={tabouret} alt="armoire de mamie" />
-          <h1>Amoire de mamie</h1>
-          <p>
-            ça pue et c'est lourd mais c'est super solide
-          </p>
-          <button>Buy</button>
-        </div>
-
-        <div className="card">
-          <img src={tabouret} alt="tabouret 60's" />
-          <h1>Tabouret 60's</h1>
-          <p>
-            100% plastique
-          </p>
-          <button>Buy</button>
-        </div>      
+    <div className="grid">
+    furArr
     </div>
+  )
+}
+
+function furniture(){
+  return (
+      <div className="card">
+        <img src={tabouret} alt="tabouret 60's" />
+        <h1>Tabouret 60's</h1>
+        <p>
+          100% plastique
+        </p>
+        <button>Buy</button>
+      </div>
   )
 }
 
