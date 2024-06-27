a4c1eb8266019162b79550a8b92e19ac14cdbca2
cetrl <celiatruel@gmail.com>
2024-06-27 11:50:16 +0200

    update node_modules attempt2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Header from './Header.jsx'
 import App from './App.jsx'
 import Footer from './Footer.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Tuto React Router</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('header')).render(
   <React.StrictMode>
